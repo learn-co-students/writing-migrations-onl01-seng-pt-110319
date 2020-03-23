7a33a35f32ec69099c8ad8acdb0882800967f20f
arnie b <arnold.bwaila@gmail.com>
2020-03-23 13:34:42 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,10 +1,12 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-  def change
-    create_table :artists do |t|
+  def up 
+  end 
+  def down
+  end
+  
+ def change
+    create_table :students do |t|
       t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
     end
   end
 end
